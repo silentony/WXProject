7b85977e6d58f22024817de4111a1ba4ed30a103
silenttoy <scrazy@qq.com>
2018-10-31 15:52:25 +0800

    添加会员实体

---
+++ Model/Member.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Member
+    {
+        public int MemberId { set; get; }
+        public string MemberName { set; get; }
+        public string MemberPassword { set; get; }
+        public DateTime CreateTime { set; get; }
+        public int MemberStatus { set; get; }
+    }
+}
